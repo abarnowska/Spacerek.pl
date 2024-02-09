8b0f935b72f3c3484c1be6186c1ab75deba1f921
Angelika Barnowska-Nawara <angelika.barnowska@gmail.com>
2024-02-09 14:51:44 +0100

    adding subtitle

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,13 +1,14 @@
-import classes from './Landingpage.module.css'
+import classes from "./Landingpage.module.css";
 
 export default function Landingpage() {
 	return (
-        <div className={classes.header}>
-            <div className={classes.heroimg}>
-            <h1>Spacerek.pl</h1>
-            </div>
-        </div>
-
-        )
-        
+		<div className={classes.header}>
+			<div className={classes.heroimg}>
+                <div className={classes.herotext}>
+				<h1>Spacerek.pl</h1>
+				<h2>SPACERUJ ZE SWOIM MALUCHEM ZDROWO I BEZPIECZNIE</h2>
+                </div>
+			</div>
+		</div>
+	);
 }
